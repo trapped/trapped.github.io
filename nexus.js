a66aaedbb956f9d42f313d8dd4fdd8352280e0ee
trapped <giorgio.pellero@gmail.com>
2014-02-28 00:59:27 +0100

    da
    da

--- nexus.js
+++ nexus.js
@@ -25,7 +25,7 @@ function getStyle() {
 function setStyle(style) {
 	localStorage["style"] = style;
 	$("body").fadeOut(function() {
-		document.getElementById("stylesheet").setAttribute("href", "http://grid12.tk/"+style+".css");
+		document.getElementById("stylesheet").setAttribute("href", "http://http://trapped.github.io//"+style+".css");
 		$(this).fadeIn();
 	});
 }
